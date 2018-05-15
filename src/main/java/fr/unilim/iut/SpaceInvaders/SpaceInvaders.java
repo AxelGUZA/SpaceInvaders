55bedcc53e24807a869fea34e1ca99af64838de9
Axel GUZA <axel.guza@etu.unilim.fr>
2018-05-15 11:22:40 +0200

    Refactoring pour le vaisseau

--- src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
@@ -6,6 +6,7 @@ public class SpaceInvaders {
 	int hauteur;
 	Vaisseau vaisseau;
 	
+	
 	public SpaceInvaders(int longueur, int hauteur) {
 		this.longueur = longueur;
 		this.hauteur = hauteur;
@@ -14,16 +15,32 @@ public class SpaceInvaders {
 	@Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
+		
 		for (int y = 0; y < hauteur; y++) {
 			for (int x = 0; x < longueur; x++) {
-				if (vaisseau!=null && vaisseau.occupeLaPosition(x, y))
-					espaceDeJeu.append('V');
-				else
-					espaceDeJeu.append('.');
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
+			    
 			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
+	}
+
+	private char recupererMarqueDeLaPosition(int x, int y) {
+		char marque;
+		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+		      marque='V';
+		else
+		      marque='.';
+		return marque;
+	}
+
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+	}
+
+	private boolean aUnVaisseau() {
+		return vaisseau!=null;
 	}
 
 	public void positionnerUnNouveauVaisseau(int x, int y) {
