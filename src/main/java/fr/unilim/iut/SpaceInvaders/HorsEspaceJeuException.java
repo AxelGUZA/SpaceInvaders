818f3a5c367727492bc0a1b1129fc2fc96865bee
gaeta <gaeta@164.81.232.63>
2018-05-22 18:10:36 +0200

    TP1 final

--- src/main/java/fr/unilim/iut/SpaceInvaders/HorsEspaceJeuException.java
+++ src/main/java/fr/unilim/iut/SpaceInvaders/HorsEspaceJeuException.java
@@ -1,12 +1,4 @@
 package fr.unilim.iut.SpaceInvaders;
-<<<<<<< HEAD
-@SuppressWarnings("serial")
-class HorsEspaceJeuException extends RuntimeException {
-
-    public HorsEspaceJeuException(String message) {
-	    super(message);
-    }
-=======
 
 @SuppressWarnings("serial")
 class HorsEspaceJeuException extends RuntimeException {
@@ -14,8 +6,6 @@ class HorsEspaceJeuException extends RuntimeException {
     public HorsEspaceJeuException(String message) {
 	    super(message);
     }
-    
-    
->>>>>>> branch 'master' of https://github.com/AxelGUZA/SpaceInvaders.git
+
 
 }
--- src/main/java/fr/unilim/iut/SpaceInvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/SpaceInvaders/Vaisseau.java
@@ -1,15 +1,13 @@
 package fr.unilim.iut.SpaceInvaders;
 
 public class Vaisseau {
-
 	int x;
 	int y;
-
+	
 	public Vaisseau(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-<<<<<<< HEAD
 
 	public boolean occupeLaPosition(int x, int y) {
 		return(this.x==x && this.y==y);
@@ -27,11 +25,5 @@ public class Vaisseau {
 	public int ordonnee() {
         return this.y;
 	}
-=======
-	
-	 public boolean occupeLaPosition(int x, int y) {
-			return (this.x==x) && (this.y==y);
-		}
->>>>>>> branch 'master' of https://github.com/AxelGUZA/SpaceInvaders.git
 
 }
