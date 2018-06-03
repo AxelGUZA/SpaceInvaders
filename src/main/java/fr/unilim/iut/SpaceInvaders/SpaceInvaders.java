f714abc2557dba87e8ac4eb0c0e593134bea88d9
Axel <Axel@192.168.0.21>
2018-06-03 19:01:06 +0200

    Fin de Fonctionnalité 4

--- src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/SpaceInvaders/SpaceInvaders.java
@@ -2,20 +2,28 @@ package fr.unilim.iut.SpaceInvaders;
 
 import Exepections.DebordementEspaceJeuException;
 import Exepections.HorsEspaceJeuException;
+import Exepections.MissileException;
 
 public class SpaceInvaders {
 	
-	private static final char MARQUE_FIN_LIGNE = '\n';
-	private static final char MARQUE_VIDE = '.';
-	private static final char MARQUE_VAISSEAU = 'V';
 	int longueur;
 	int hauteur;
 	Vaisseau vaisseau;
+	Missile missile;
 	
 	public SpaceInvaders(int longueur, int hauteur) {
 		this.longueur = longueur;
 		this.hauteur = hauteur;
 	}
+	
+	  public void tirerUnMissile(Dimension dimensionMissile, int vitesseMissile) {
+			
+		   if ((vaisseau.hauteur()+ dimensionMissile.hauteur()) > this.hauteur )
+			   throw new MissileException("Pas assez de hauteur libre entre le vaisseau et le haut de l'espace jeu pour tirer le missile");
+							
+		   this.missile = this.vaisseau.tirerUnMissile(dimensionMissile,vitesseMissile);
+      }
+   
 
 	@Override
 	public String toString() {
@@ -24,7 +32,7 @@ public class SpaceInvaders {
 			for (int x = 0; x < longueur; x++) {
 				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
-			espaceDeJeu.append(MARQUE_FIN_LIGNE);
+			espaceDeJeu.append(Constante.MARQUE_FIN_LIGNE);
 		}
 		return espaceDeJeu.toString();
 	}
@@ -32,10 +40,21 @@ public class SpaceInvaders {
 	private char recupererMarqueDeLaPosition(int x, int y) {
 		char marque;
 		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
-			marque=MARQUE_VAISSEAU;
+			marque= Constante.MARQUE_VAISSEAU;
+		else if (this.aUnMissileQuiOccupeLaPosition(x, y))
+			marque = Constante.MARQUE_MISSILE;
 		else
-			marque=MARQUE_VIDE;
+			marque=Constante.MARQUE_VIDE;
 		return marque;
+	}
+
+	private boolean aUnMissileQuiOccupeLaPosition(int x, int y) {
+		return aUnMissile() && missile.occupeLaPosition(x, y);
+	}
+	
+
+	private boolean aUnMissile() {
+		return missile!=null;
 	}
 
 	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
@@ -83,7 +102,7 @@ public class SpaceInvaders {
             for (int x = 0; x < longueur; x++) {
                 espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
             }
-            espaceDeJeu.append(MARQUE_FIN_LIGNE);
+            espaceDeJeu.append(Constante.MARQUE_FIN_LIGNE);
         }
         return espaceDeJeu.toString();
     }
@@ -95,6 +114,8 @@ public class SpaceInvaders {
 			vaisseau.positionner(0, vaisseau.ordonneeLaPlusHaute());
 		}
 	}
+	
+	
 	
 	
 }
